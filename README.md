cb0a5a898de7faae18fa0fd7f2362b9b85bb248f
Caio Tavares <caio.tavares11@gmail.com>
2020-10-31 19:39:30 -0300

    Update README and add datasheet

--- README.md
+++ README.md
@@ -6,7 +6,9 @@
 
 ## Setup
 
-- Install the compiler and SDK
+- Install the compiler, SDK and OpenOCD for debugging
+  - `brew install armmbed/formulae/arm-none-eabi-gcc`
+  - `brew install openocd`
 - Export `GCC_ARM_HOME` and `ARM_SDK_HOME` environment variables pointing to the compiler and SDK homes, respectively
 - Install the flash burner `Uniflash` and setup the burn settings for the board, go to `Standalone Command line tool` and export a binary with the selected configurations.
 - Export `ARM_BURNER` environment variable with the exported binary above
--- src/main.c
+++ src/main.c
@@ -7,20 +7,22 @@ int main(void)
     // Stop watchdog timer
     WDT_A_hold(WDT_A_BASE);
 
-    // Set P1.0 to output direction
-    GPIO_setAsOutputPin(
-        GPIO_PORT_P1,
-        GPIO_PIN0);
+    // Set P1.0 and P2.2 to output direction
+    GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
+    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN2);
+
+    GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
+    GPIO_toggleOutputOnPin(GPIO_PORT_P2, GPIO_PIN2);
 
     while (1)
     {
-        // Toggle P1.0 output
-        GPIO_toggleOutputOnPin(
-            GPIO_PORT_P1,
-            GPIO_PIN0);
+        // Toggle P1.0 and P2.2 output
+        GPIO_toggleOutputOnPin(GPIO_PORT_P1, GPIO_PIN0);
 
         // Delay
-        for (i = 100000; i > 0; i--)
+        for (i = 500000; i > 0; i--)
             ;
+
+        GPIO_toggleOutputOnPin(GPIO_PORT_P2, GPIO_PIN2);
     }
 }
